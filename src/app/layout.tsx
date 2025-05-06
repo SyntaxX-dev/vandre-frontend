a5a302d94a83d0d473bec48a212248c2b57114b5
gabrielglp <gabriel_pg08@hotmail.com>
2025-05-06 19:34:15 -0300

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 // Using the correct metadata type from Next.js
 export const metadata: Metadata = {
   title: "Turma do Vandré - Agência de Turismo",
-  description: "Experimente a magia de explorar os destinos mais deslumbrantes do mundo com os nossos pacotes de viagem personalizados para todos os aventureiros.",
+  description: "Experimente a magia de explorar os destinos mais deslumbrantes do mundo com os nossos pacotes de viagens personalizados para todos os aventureiros.",
 };
 
 export default function RootLayout({ 
