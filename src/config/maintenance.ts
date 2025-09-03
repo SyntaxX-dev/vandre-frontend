cfc2ea35863fcb6e1564531a6d1b0b20ed4a7348
Breno-Lima <breno.souza@academico.ufpb.br>
2025-09-03 14:59:56 -0300

    fix

--- src/config/maintenance.ts
+++ src/config/maintenance.ts
@@ -1,7 +1,7 @@
 // Configuração de manutenção do site
 export const MAINTENANCE_CONFIG = {
   // Defina como true para ativar o modo de manutenção
-  isMaintenanceMode: true,
+  isMaintenanceMode: false,
   
   // Mensagem personalizada para a página de manutenção
   maintenanceMessage: "Estamos trabalhando para melhorar sua experiência",
